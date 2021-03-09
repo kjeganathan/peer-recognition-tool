387960bb22d11fade2b2cb08c9a5b1b8ae27417b
briansun1210 <44716335+briansun1210@users.noreply.github.com>
2021-03-09 00:08:05 -0500

    adding user post layout

--- prt-frontend/src/components/Large/Home.js
+++ prt-frontend/src/components/Large/Home.js
@@ -2,12 +2,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import UserPostLayOut from "../Medium/UserPostLayOut";
 
 export default class Home extends Component {
   render(){
     return (
     <Router>
         <div>
+          <UserPostLayOut/>
         </div>
     </Router>
     );
