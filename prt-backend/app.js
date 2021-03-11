fd9d4c1ed8411f6b886996456b1c45b32c2a7a09
Langchun Zhou <zhoulangchun@gmail.com>
2021-03-11 17:23:02 -0500

    Fetched remote master

--- prt-backend/app.js
+++ prt-backend/app.js
@@ -5,13 +5,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 //get request to '/employee' using res.send inside
-var recognitions = {
-  recognizer: null,
-  recognizee: null,
-  core: null,
-  message: null,
-}
+// var recognitions = {
+//   recognizer: null,
+//   recognizee: null,
+//   core: null,
+//   message: null,
+// }
 
+app.use(express.static(__dirname + "/../prt-frontend/public/"));
+
+app.get("/", function(request, response){
+  console.log("Index requested.");
+})
 
 app.use(cors());
 app.use(bodyParser.json());
