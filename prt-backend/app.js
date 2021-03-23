901357c9aca544e790def0672740fda10c84dc32
elnott2142 <45403807+elnott2142@users.noreply.github.com>
2021-03-23 19:04:19 -0400

    Changes minus lenny working

--- prt-backend/app.js
+++ prt-backend/app.js
@@ -1,47 +1,123 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const config = require("./config.json");
 
-const session = require("express-session");
-const mongoose = require("mongoose");
-
-const passport = require("./passport.js"); //Presentation note 1: all the code for
-                                           //authentication has been moved to passport.js
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const session = require("express-session")
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const { response } = require('express')
+const MongoClient = require('mongodb').MongoClient;
 
 const SESSION_LENGTH = 1_800_000;  // = 30 minutes in ms
-const databaseURI = config.URI;
+const URI = "mongodb+srv://devapp:wintermute3000@cluster0.val9t.mongodb.net/TestDatabase"
+  + "?retryWrites=true&w=majority";
 
-mongoose //Presentation note 2: we are now using Mongoose again, because we've reached the
-         //point where the extra complexity is worth it
-  .connect(databaseURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(console.log("Connected to MongoDB database."))
-  .catch(error => console.log(error));
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 app.use(session({ secret: 'compsci320', maxAge: SESSION_LENGTH }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(bodyParser.json());
+app.options('*', cors())
 
-app.post("/login", //Presentation note 3: use passport.js's authentication function when
-                   //we receive a post request from the login page
-  passport.authenticate("local"),
-  (req, res) => { //called only on success
-    console.log("Logging in.");
-    res.send({ user: req.user });
-  }
-);
+//recognitiom and employee structure
+var recognitions = {
+  recognizer: null,
+  recognizee: null,
+  core: null,
+  message: null,
+}
 
-// app.get('/testlogin', (req, res) => {
-//   // Check if the user is logged in by calling req.isAuthenticated().
-//   if (!req.isAuthenticated()) {
-//     res.status(401).send({ message: 'You are not logged in' });
-//   } else {
-//     res.send({ message: 'Congratulations, you are logged in', user: req.user });
-//   }
-// });
+var employee = {
+    _id: null,
+    firstName: null,
+    lastName: null,
+    companyId: null,
+    password: null,
+    positionTitle: null,
+    companyName: null,
+    isManager: null,
+    employeeId: null,
+    managerId: null,
+    email: null,
+    startDate: null,
+    recognitionsGiven: null,
+    recognitionsReceived: null
+}
+
+// Calling passport.use tells Passport to run this code to match a username and
+// password to a user.
+//    See http://www.passportjs.org/docs/configure/
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  async function(username, password, done) {
+      const client = new MongoClient(URI);
+      try {
+          await client.connect();
+          var dbo = client.db("Test-Database");
+          dbo.collection("Employees").findOne({email: username}, function(err, result) {
+          if( result ==null){
+            return done(null, false, {
+              message: 'WIP, use username = "username" and password = "password"'
+            });
+          }
+                // TODO: When connected with database, actually look up a user.
+          if (username === result.email && password === result.password) {
+          // We would want to return a user from the database here
+            return done(null, result);
+          }
+
+          // Returning done with false indicates that the credentials were incorrect
+          return done(null, false, {
+            message: 'WIP, use username = "username" and password = "password"'
+          });
+          });
+        }
+      finally {
+        await client.close();
+      }
+    }
+));
+
+
+passport.serializeUser(function(user, done) {
+  // TODO: Use database and return user ID
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+/**
+ * @openapi
+ * /login:
+ *   post:
+ *     description: Log in with a given username and password
+ *     parameters:
+ *       -
+ *          name: credentials
+ *          in: body
+ *          description: Username and password
+ *          required: true
+ *          schema:
+ *              type: object
+ *              required:
+ *                - username
+ *                - password
+ *              properties:
+ *                username:
+ *                  type: string
+ *                password:
+ *                  type: string
+ */
+app.post('/login', passport.authenticate('local'), (req, res) => {
+  res.send({ message: 'Logged in successfully', user: req.user });
+});
+
 
 app.options('*', cors())
 app.get("/recogs", (req, res) => {
@@ -98,10 +174,17 @@ async function getUser(req, res) {
   try {
     await client.connect();
     var dbo = client.db("Test-Database");
-    dbo.collection("TestEmployees").findOne({companyId: req.body.id}, function(err, result) {
-      console.log(result);
-      res.send(result)
+    var id1;
+    var id2;
+    dbo.collection("Employees").findOne({employeeId: req.body.id1}, function(err, result) {
+      
+      id1 = result
     });
+    dbo.collection("Employees").findOne({employeeId: req.body.id2}, function(err, result) {
+      id2 = result
+    });
+    console.log({1:id1, 2:id2})
+    res.send({1:id1, 2:id2})
   }
   finally {
     await client.close();
