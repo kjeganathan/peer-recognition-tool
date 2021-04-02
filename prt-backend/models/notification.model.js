f600979f1b1faf1535feb9ce492f330d0652753d
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-02 15:29:55 -0400

    Added notification model

---
+++ prt-backend/models/notification.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const notificationSchema = new Schema(
+    {
+        message:        String,
+        arrivalTime:    Date,
+        recognitionID:  [mongoose.ObjectId]
+    }, {
+        collection: "Notifications"
+    }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema, "Notifications");
+
+module.exports = Notification;
