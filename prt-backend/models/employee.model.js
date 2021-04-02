3f833e1adf33e500ce789e7a355a8d7243fca747
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-02 14:46:02 -0400

    Fetched and merged from lenny's-branch

---
+++ prt-backend/models/employee.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema(
+    {
+        firstName:              String,
+        lastName:               String,
+        companyId:              Number,
+        password:               String,
+        positionTitle:          String,
+        companyName:            String,
+        isManager:              Boolean,
+        employeeId:             Number,
+        managerId:              Number,
+        email:                  String,
+        startDate:              String,
+        recognitionsGiven:      [mongoose.ObjectId],
+        recognitionsReceived:   [mongoose.ObjectId]
+    }, {
+        collection: "Employees"
+    }
+)
+
+//mongoose.model(ModelName, schema, ModelCollection)
+const Employee = mongoose.model("Employee", employeeSchema, "Employees");
+
+module.exports = Employee;
---
+++ prt-backend/models/recognition.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recognitionSchema = new Schema(
+    {
+        companyID:              Number,
+        giverName:              String,
+        receiverName:           String,
+        giverID:                Number,
+        receiverID:             Number,
+        values:                 [String],
+        message:                String
+    }, {
+        collection: "Recognitions"
+    }
+)
+
+//mongoose.model(ModelName, schema, ModelCollection)
+const Recognition = mongoose.model("Recognition", recognitionSchema, "Recognitions");
+
+module.exports = Recognition;
