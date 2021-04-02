a7056e0c453ebfee02003f8592f2bda011df6aa5
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-02 14:42:11 -0400

    Created and displayed getUser()

--- prt-backend/mongoCalls/user.js
+++ prt-backend/mongoCalls/user.js
@@ -2,26 +2,31 @@ const MongoClient = require('mongodb').MongoClient;
 const SESSION_LENGTH = 1_800_000;  // = 30 minutes in ms
 const URI = "mongodb+srv://devapp:wintermute3000@cluster0.val9t.mongodb.net/TestDatabase"
   + "?retryWrites=true&w=majority";
+const Employee = require("../models/employee.model.js");
 
-async function getUser(req, res) {
-    const client = new MongoClient(URI);
-    try {
-      var user;
-      await client.connect();
-      var dbo = client.db("Test-Database");
-      dbo.collection("Employees").findOne({employeeId: req.body.id}, function(err, result) {
-        user = result;
-        console.log(result);
-      });
-      console.log(user)
-      return user;
-    }
-    finally {
-      await client.close();
-    }
-  }
+// async function getUser(req, res) {
+//   const client = new MongoClient(URI);
+//   try {
+//     var user;
+//     await client.connect();
+//     var dbo = client.db("Test-Database");
+//     dbo.collection("Employees").findOne({ employeeId: req.body.id }, function (err, result) {
+//       user = result;
+//       console.log(result);
+//     });
+//     console.log(user)
+//     return user;
+//   }
+//   finally {
+//     await client.close();
+//   }
+// }
 
+async function getUser(req, res){
+  const employeeID = req.user.id; // Watch out for capitalization of "ID"
+  console.log("Finding user with ID " + employeeID + ".");
+  const user = await Employee.findOne({employeeId: employeeID});
+  return user;
+}
 
-
-
-  module.exports = {getUser}
+module.exports = { getUser }
--- prt-frontend/src/components/Medium/ProfilePage.js
+++ prt-frontend/src/components/Medium/ProfilePage.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from "react";
-import { Redirect, BrowserRouter as Router, Route} from "react-router-dom";
+import { Redirect, BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import profilePic from "./genericProfilePicture.jpeg";
 import './ProfilePage.css';
@@ -8,30 +8,30 @@ import axios from 'axios';
 import { AiOutlineConsoleSql } from "react-icons/ai";
 
 export default class ProfilePage extends Component {
-  constructor(props){
-    super(props);    
+  constructor(props) {
+    super(props);
 
 
 
     this.state = {
-        // username: props.user.fullName, 
-        // email: props.user.email,
-        // company: props.user.company,
-        // position: props.user.position
+      // username: props.user.fullName, 
+      // email: props.user.email,
+      // company: props.user.company,
+      // position: props.user.position
 
-        username: localStorage.getItem('fullName'), 
-        email: localStorage.getItem('email'),
-        company: localStorage.getItem('company'),
-        position: localStorage.getItem('position')
+      username: localStorage.getItem('fullName'),
+      email: localStorage.getItem('email'),
+      company: localStorage.getItem('company'),
+      position: localStorage.getItem('position')
     }
     // console.log(this.props);
 
     // console.log(this.props.state)
-  } 
+  }
 
 
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState((state) => ({
       username: state.username,
       email: state.email,
@@ -42,44 +42,44 @@ export default class ProfilePage extends Component {
   }
 
 
-  profile(){
-    return <div class = "relative"> 
-            <div class= "entireProfile">
-              <div class = "fade-in"> 
-                <div className= "profile">       
-                  <div className = "userInfo">
+  profile() {
+    return <div class="relative">
+      <div class="entireProfile">
+        <div class="fade-in">
+          <div className="profile">
+            <div className="userInfo">
 
-                      <div className = "profileHeader">   
-                        <p id= "name"><strong>{this.state.username}</strong></p>
-                      </div>
-
-                      <div className = "details">
-                        <p class = "info" ><strong>Email: </strong><i> {this.state.email}</i> </p>
-                        <p class = "info" ><strong>Company: </strong>{this.state.company} </p>
-                        <p class = "info"> <strong>Position: </strong>{this.state.position} </p>
-                      </div>
-
-                  </div>
-                </div>
+              <div className="profileHeader">
+                <p id="name"><strong>{this.state.username}</strong></p>
               </div>
+
+              <div className="details">
+                <p class="info" ><strong>Email: </strong><i> {this.state.email}</i> </p>
+                <p class="info" ><strong>Company: </strong>{this.state.company} </p>
+                <p class="info"> <strong>Position: </strong>{this.state.position} </p>
+              </div>
+
             </div>
           </div>
-  }
-
-  handleShow = ()=>{ //this is for the post button for open the textarea 
-        this.setState({
-            isActive: !this.state.isActive
-        }) 
-        console.log(this.state.isActive)
-  }
-
-  render(){
-    return (
-        <div>
-          {this.state.isActive && this.profile()}
-          <button class = "open" onClick = {this.handleShow}></button> 
         </div>
+      </div>
+    </div>
+  }
+
+  handleShow = () => { //this is for the post button for open the textarea 
+    this.setState({
+      isActive: !this.state.isActive
+    })
+    console.log(this.state.isActive)
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isActive && this.profile()}
+        <button class="open" onClick={this.handleShow}></button>
+      </div>
     );
- }
+  }
 
 }
