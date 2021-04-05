8e86ef0f191baae9f1cecbc18499c6bce870d83e
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-05 16:24:07 -0400

    Refactored notifications

--- prt-backend/notifications.js
+++ prt-backend/notifications.js
@@ -1,39 +1,44 @@
-// This file contains functions and endpoints for working with notifications.
 const express = require('express')
 const mongoose = require('mongoose')
 
-// Routers are used to split endpoints across multiple files.
 const router = express.Router()
 
-/**
- * @openapi
- * /notifications:
- *   get:
- *     description: Get notifications for a given employee
- *     responses:
- *       '200':
- *         description: A list of unread recognitions for a given employee.
- */
- router.get('/', async (req, res) => {
+// Handle GET request for notification
+router.get('/', async (req, res) => {
     if (!req.isAuthenticated()) {
         res.status(401).send({ message: 'You are not logged in' })
         return
     }
 
-    // While notification scheduler is WIP, send some test
-    // notifications for frontend to test with
-    res.status(200).send([
-      {
-          message:       'Test Notification 0',
-          arrivalTime:   Date.now(),
-          recognitionID: 0
-      },
-      {
-          message:       'Test Notification 1',
-          arrivalTime:   Date.parse('04 Dec 1995 00:12:00 GMT'),
-          recognitionID: 1
-      },
-    ])
+    // res.status(200).json([
+    //     {
+    //         message: 'Test Notification 0',
+    //         arrivalTime: Date.now(),
+    //         recognitionID: 0
+    //     },
+    //     {
+    //         message: 'Test Notification 1',
+    //         arrivalTime: Date.parse('04 Dec 1995 00:12:00 GMT'),
+    //         recognitionID: 1
+    //     },
+    // ])
+
+    res.status(200).json(
+        {
+            notifications: [
+                {
+                    message: "Test notification message 0",
+                    arrivalTime: new Date(),
+                    recognitionID: 0
+                },
+                {
+                    message: "Test notification message 1",
+                    arrivalTime: new Date("2021-01-01T00:00:00"),
+                    recognitionID: 1
+                }
+            ]
+        }
+    );
 
     // FIXME: Replace above placeholder data with production data below (works)
     // res.status(200).send(req.user.activeNotifications);
--- prt-frontend/src/components/Medium/Notifications.js
+++ prt-frontend/src/components/Medium/Notifications.js
@@ -1,90 +1,89 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 
 import { BiBell } from 'react-icons/bi';
-import {SignOutIcon, BellIcon  } from '@primer/octicons-react'
+import { SignOutIcon, BellIcon } from '@primer/octicons-react'
 import './Notifications.css';
 
 import axios from 'axios';
 
 
-export default class Notifications extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-          newNotifications: 4,
-          elements: []
-        };
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.updateNotifications = this.updateNotifications.bind(this);
-        console.log(this.props)
+export default class Notifications extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numNewNotifications: 4,
+      elements: []
+    };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateNotifications = this.updateNotifications.bind(this);
+    console.log(this.props)
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/notifications', { withCredentials: true })
+      .then((res) => this.updateNotifications(res));
+  }
+
+  updateNotifications(res) {
+    const newNotifications = res.data.notifications;
+    const newElements = newNotifications.map(notification => {
+      return [
+        <Dropdown.Item href="#/mention">
+          {notification.message}
+          <p>{notification.arrivalTime}</p>
+        </Dropdown.Item>,
+        <Dropdown.Divider></Dropdown.Divider>
+      ];
+    });
+    // console.log("Notifications: " + notifications);
+
+    this.setState({
+      numNewNotifications: newNotifications.length,
+      elements: newElements
+      // elements: res.body.flatMap((notification) => {
+      //   return [
+      //     <Dropdown.Item href="#/mention">{notification.message}<p>{notification.arrivalTime}</p> </Dropdown.Item>,
+      //     <Dropdown.Divider />
+      //   ];
+      // })
+    });
+  }
+
+  CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <button class="notificationButton jelly"
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+        this.setState((state) => { return { numNewNotifications: 0 } });
+        this.componentDidMount();
+      }}
+    >
+      {children}
+    </button>
+  ));
+
+  render() {
+    let badge = null;
+    if (this.state.numNewNotifications > 0) {
+      badge = <span class="badge badge-pill x2">{this.state.numNewNotifications}</span>;
     }
-    
-    componentDidMount() {
-      axios.get('http://localhost:3001/notifications', { withCredentials: true })
-          .then((res) => this.updateNotifications(res));
-    }
+    return (
+      <Dropdown>
+        <Dropdown.Toggle as={this.CustomToggle}>
+          <BiBell size={30} />
+          {/* <BellIcon size={30}/> */}
+          {badge}
+        </Dropdown.Toggle>
 
-    updateNotifications(res) {
-      // res.body is [
-      //   {
-      //     message: "Jiachen ding was recognized by god",
-      //     arrivalTime: Date.now(),
-      //     recognitionID: "23dfvhaosdgaeorghafb"
-      //   },
-      // ]
-
-      // npm -v
-
-
-      // set this.state.newNotifications = length of res.body
-      // for each notification object in res.body
-      //    add a notification to the dropdown
-
-      this.setState({
-        newNotifications: res.body.length(),
-        elements: res.body.flatMap((notification) => {
-          return [
-            <Dropdown.Item href="#/mention">{notification.message}<p>{notification.arrivalTime}</p> </Dropdown.Item>,
-            <Dropdown.Divider/>
-          ];
-        })
-      });
-    }
-
-    CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-        <button class = "notificationButton jelly"
-          ref={ref}
-          onClick={(e) => {
-            e.preventDefault();
-            onClick(e);
-            this.setState((state)=>{return{newNotifications:0}});
-            this.componentDidMount();
-          }}
-        >
-          {children}
-        </button>
-      ));
-      
-    render() {
-        let badge = null;
-        if(this.state.newNotifications > 0){
-            badge =<span class="badge badge-pill x2">{this.state.newNotifications}</span>;
-        }
-        return (
-        <Dropdown>
-            <Dropdown.Toggle as={this.CustomToggle}>
-                <BiBell size = {30}/>  
-                {/* <BellIcon size={30}/> */}
-                {badge}
-            </Dropdown.Toggle>  
-            
-            <Dropdown.Menu> 
-            <Dropdown.Header>Notifications</Dropdown.Header>
-              {this.state.elements}
-            </Dropdown.Menu>
-        </Dropdown>
-        );
-      }
+        <Dropdown.Menu>
+          <Dropdown.Header>Notifications</Dropdown.Header>
+          {this.state.elements}
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
 }
 
