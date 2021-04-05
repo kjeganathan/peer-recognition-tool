9c360e206b6470bdb326c9e01b9dde45050d59fa
dhsavell <dhsavell05@gmail.com>
2021-04-05 13:15:08 -0400

    Implement database connection and placeholder data for /notifications

--- prt-backend/notifications.js
+++ prt-backend/notifications.js
@@ -1,5 +1,6 @@
 // This file contains functions and endpoints for working with notifications.
 const express = require('express')
+const mongoose = require('mongoose')
 
 // Routers are used to split endpoints across multiple files.
 const router = express.Router()
@@ -18,8 +19,37 @@ const router = express.Router()
         res.status(401).send({ message: 'You are not logged in' })
         return
     }
-    console.log("Test");// FIXME 
-    res.status(200).send({message: 'placeholder'})
+
+    // While notification scheduler is WIP, send some test
+    // notifications for frontend to test with
+    res.status(200).send([
+      {
+          message:       'Test Notification 0',
+          arrivalTime:   Date.now(),
+          recognitionID: 0
+      },
+      {
+          message:       'Test Notification 1',
+          arrivalTime:   Date.parse('04 Dec 1995 00:12:00 GMT'),
+          recognitionID: 1
+      },
+    ])
+
+    // FIXME: Replace above placeholder data with production data below (works)
+    // res.status(200).send(req.user.activeNotifications);
+})
+
+router.delete('/', async (req, res) => {
+    if (!req.isAuthenticated()) {
+        res.status(401).send({ message: 'You are not logged in' })
+        return
+    }
+
+    // FIXME: Uncomment these lines when line 38 is addressed
+    // req.user.notifications = [];
+    // await req.user.save();
+
+    res.status(204);
 })
 
 module.exports = router;
