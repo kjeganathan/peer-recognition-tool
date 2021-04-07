5ec53215e584b184ee50ce5c12b0d7cddc93bc3d
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-07 01:40:34 -0400

    Killing bugs

--- prt-backend/mongoCalls/recognitions.js
+++ prt-backend/mongoCalls/recognitions.js
@@ -9,68 +9,31 @@ async function getRecognitionsFromCompany(req, res) {
 }
 
 async function postRecognition(req, res) {
-  // const receiverName = req.body.receiverName;
-  // // console.log(receiverName);
+  const newRecognition = req.body;
+  console.log("newRecognition: ");
+  console.log(newRecognition);
 
-  // const receiverNameTokens = receiverName.split(" ");
-  // // console.log(receiverNameTokens);
+  await fillReceiverValues(req.body);
+  console.log("\nnewRecognition after fillReceiverValues(): ");
+  console.log(newRecognition);
 
-  // const firstName = receiverNameTokens[0];
-  // // console.log(firstName);
+  // const newRecognition = new Recognition(req.body);
+  // fillReceiverValues(newRecognition); //Won't be necessary after better input validation
 
-  // const lastName = receiverNameTokens[1];
-  // // console.log(lastName);
-  //   db.Recognitions.find().forEach(function (document) {
-  //     db.Recognitions.update(
-  //         { _id: document._id },
-  //         {
-  //             $set:
-  //             {
-  //                 receiverProfilePicURL: getProfilePicURL(
-  //                     document.receiverName,
-  //                     document.companyID
-  //                 )
-  //             }
-  //         }
-  //     );
-  // });
-
-
-  const newRecognition = new Recognition(req.body);
-  // console.log("newRecognition: ");
-  // console.log(newRecognition);
-
-  setReceiverValues(newRecognition); //Won't be necessary after better input validation
-
-  newRecognition.save()
+  // newRecognition.save()
+  new Recognition(newRecognition).save()
     .then(() => res.send("Recognition posted."))
     .catch(error => res.status(400).send("Error: " + error));
 }
 
-
-// Retooled from prt-database-scripts/addAssociatedProfilePicURLs.js
-// function setReceiverValues(fullName, companyID) {
-  // Won't be necessary after better input validation
-function setReceiverValues(newRecognition) {
-  // console.log("Executing setReceiverValues() on: ");
-  // console.log(newRecognition);
-
+async function fillReceiverValues(newRecognition) {
+  // const newRecognition = {};
+  // Object.assign(newRecognition, recognition);
   const fullName = newRecognition.receiverName;
-  // console.log("fullName: " + fullName);
-
   const companyID = newRecognition.companyID;
-  // console.log("companyID: " + companyID);
-
   const nameTokens = fullName.split(" ");
-  // console.log("nameTokens: " + nameTokens);
 
   if (nameTokens.length != 2) {
-    // return {
-    //   receiverID: -1,
-    //   receiverProfile: "test-profile-pic-10.jpg"
-    // }
-    // newRecognition.receiverID = -1;
-    // newRecognition.receiverProfile = "test-profile-pic-10.jpg";
     setPlaceholders(newRecognition);
     return;
   }
@@ -92,16 +55,13 @@ function setReceiverValues(newRecognition) {
         console.log("Writing non-placeholder values");
         newRecognition.receiverID = employee.employeeId; //watch out for capitalization of employeeID
         newRecognition.receiverProfilePicURL = employee.profilePicURL;
+        console.log("newRecognition: ");
+        console.log(newRecognition);
       }
     })
     .catch(error => console.log(error));
-  // const employee = db.Employees.findOne(query)
 
-  // if (employee == null) {
-  //   return "test-profile-pic-10.jpg";
-  // }
-
-  // return employee.profilePicURL;
+  // return newRecognition;
 }
 
 function setPlaceholders(newRecognition) {
