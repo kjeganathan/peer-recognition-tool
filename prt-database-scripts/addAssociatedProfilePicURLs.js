fb9c6960ba18335560fdb7edbecb24ca3cc0fb60
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-07 00:30:41 -0400

    Added associated profile pic URLs to recognitions

--- prt-database-scripts/addAssociatedProfilePicURLs.js
+++ prt-database-scripts/addAssociatedProfilePicURLs.js
@@ -1,70 +1,58 @@
-// const minDate = new Date("2020-01-01T00:00:00");
-// const maxDate = new Date();
-
 db.Recognitions.find().forEach(function (document) {
-    // db.Recognitions.update(
-    //     {_id: document._id},
-    //     {$set:
-    //         {
-    //             receiverProfilePicURL: getRandomDate(minDate, maxDate)
-    //         }
-    //     }
-    // )
-    // helper(document)
-    setProfilePicURL(document);
-});
-
-function setProfilePicURL(document) {
-    const fullName = document.receiverName;
-    const nameTokens = fullName.split(" ");
-
-    if (nameTokens.length != 2) {
-        // return "test-profile-pic-10.jpg";
-        helper(document._id, "test-profile-pic-10.jpg");
-        return;
-    }
-
-    const companyID = document.companyID;
-    const firstName = nameTokens[0];
-    const lastName = nameTokens[1];
-    const query = 
-    const employee = db.Employees.findOne({ firstName: firstName, lastName: lastName })
-    // // .then()
-    //     .then(callback)
-    //     .catch(error => print(error));
-
-    if (employee == null) {
-        helper(document._id, "test-profile-pic-10.jpg");
-        return;
-    }
-    
-    helper(document._id, employee.profilePicURL);
-    return;
-}
-
-function helper(documentID, URL) {
     db.Recognitions.update(
-        { _id: documentID },
+        { _id: document._id },
         {
             $set:
             {
-                receiverProfilePicURL: URL
+                receiverProfilePicURL: getProfilePicURL(
+                    document.receiverName,
+                    document.companyID
+                )
             }
         }
-    )
+    );
+    // setProfilePicURL(document);
+});
+
+function getProfilePicURL(fullName, companyID) {
+    // const fullName = document.receiverName;
+    const nameTokens = fullName.split(" ");
+
+    if (nameTokens.length != 2) {
+        // helper(document._id, "test-profile-pic-10.jpg");
+        // return;
+        return "test-profile-pic-10.jpg";
+    }
+
+    // const companyID = document.companyID;
+    const firstName = nameTokens[0];
+    const lastName = nameTokens[1];
+
+    const query = {
+        companyId: companyID,
+        firstName: firstName,
+        lastName: lastName
+    }
+
+    const employee = db.Employees.findOne(query)
+
+    if (employee == null) {
+        // helper(document._id, "test-profile-pic-10.jpg");
+        return "test-profile-pic-10.jpg";
+    }
+    
+    // helper(document._id, employee.profilePicURL);
+    return employee.profilePicURL;
 }
 
-function callback(employee) {
+function helper(documentID, URL) {
+    // db.Recognitions.update(
+    //     { _id: documentID },
+    //     {
+    //         $set:
+    //         {
+    //             receiverProfilePicURL: URL
+    //         }
+    //     }
+    // )
 }
-
-// function getRandomDate(minDate, maxDate){
-//     const minDateTime = minDate.getTime();
-//     const maxDateTime = maxDate.getTime();
-//     const randomDateTime = databaseHelper.getRandomInteger(minDateTime, maxDateTime);
-//     return new Date(randomDateTime);
-// }
-
-// function getRandomInteger(min, max){
-//     const range = max - min;
-//     return Math.floor(Math.random() * (range + 1)) + min;
-// }
