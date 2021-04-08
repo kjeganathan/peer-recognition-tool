f5d2fdd75ba30e5bfbba823b6cfa9988e7e2e3b0
kazuhaokamoto <kazuhaokamoto2018@gmail.com>
2021-04-08 13:22:27 -0400

    integrated with frontend

--- prt-backend/app.js
+++ prt-backend/app.js
@@ -119,7 +119,7 @@ app.post("/postRec", (req, res) => {
 app.use('/awards', require('./routes/awards'));
 app.use('/notifications', require('./routes/notifications'));
 app.use('/core-values', require('./routes/core-values'));
-app.use('/rockstars', require('./routes/rockstar.js'));
+app.use('/rockstars', require('./routes/Rockstars.js'));
 
 // The call to app.listen(PORT, ...) is in server.js
 module.exports = app
