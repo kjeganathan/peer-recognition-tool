1ee958720e934274a3410626d20e9e993861896a
justinross5 <jwross@umass.edu>
2021-04-15 18:23:44 -0400

    Awards button debug

--- prt-frontend/src/components/Medium/AwardsButton.js
+++ prt-frontend/src/components/Medium/AwardsButton.js
@@ -7,7 +7,27 @@ import Overlay from "react-bootstrap/Overlay";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export default class AwardsButton extends Component{
-   
+   constructor(props) {
+       super(props)
+       this.state = {
+            thumbsUp: 0,
+            goat: 0,
+            laugh: 0,
+            love: 0
+       }
+   }
+
+    handleButtonClick(type) {
+       if(this.type == "thumbsUp") {
+           this.setState({thumbsUp: this.thumbsUp + 1});
+       } else if(this.type == "goat") {
+           this.setState({goat: this.goat + 1});
+       } else if(this.type == "laugh") {
+           this.setState({laugh: this.laugh + 1});
+       } else {
+           this.setState({love: this.love + 1});
+       }
+   }
     render(){
         return(
             <div class="share-button">
@@ -23,12 +43,12 @@ export default class AwardsButton extends Component{
                     placement={placement}
                     overlay={
                         <Tooltip id={`tooltip-${placement}`}>
-                       <strong>Carry</strong>
+                       <strong>Thumbs Up</strong>
                         </Tooltip>
                     }
                     >
-                    <Button className="buttons">
-                        <badge className="badge1" >2</badge>
+                    <Button onClick={this.handleButtonClick("thumbsUp")} className="buttons">
+                    {(this.thumbsUp > 0) ? <badge className="badge1" >{this.thumbsUp}</badge> : null}
                         <img src="https://img.icons8.com/color/20/000000/flex-biceps.png"/>
                         
                     </Button>
@@ -48,8 +68,8 @@ export default class AwardsButton extends Component{
                         </Tooltip>
                     }
                     >
-                    <Button className="buttons">
-                        <badge className="badge2" >5</badge>
+                    <Button onClick={this.handleButtonClick("goat")} className="buttons">
+                    {(this.goat > 0) ? <badge className="badge2" >{this.goat}</badge> : null}
                         <img src="https://img.icons8.com/color/20/000000/pet.png"/>
                         
                     </Button>
@@ -64,13 +84,13 @@ export default class AwardsButton extends Component{
                     placement={placement}
                     overlay={
                         <Tooltip id={`tooltip-${placement}`}>
-                        <strong>Pog</strong>
+                        <strong>Laugh</strong>
                         </Tooltip>
                     }
                     >
-                    <Button className="buttons">
+                    <Button onClick={this.handleButtonClick("laugh")} className="buttons">
                         
-                            <badge className="badge3" >6</badge>
+                    {(this.laugh > 0) ? <badge className="badge3" >{this.laugh}</badge> : null}
                         
                         <img src="https://img.icons8.com/color/20/000000/pug.png"/>
                         
@@ -93,8 +113,8 @@ export default class AwardsButton extends Component{
                         </Tooltip>
                     }
                     >
-                    <Button className="buttons">
-                        <badge className="badge4" >4</badge>
+                    <Button onClick={this.handleButtonClick("love")} className="buttons">
+                        {(this.love > 0) ? <badge className="badge4" >{this.love}</badge> : null}
                         <img src="https://img.icons8.com/color/20/000000/heart-with-arrow--v2.png"/>
                         
                     </Button>
