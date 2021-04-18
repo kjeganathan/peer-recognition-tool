8713b90743730c891a0fbb83c162bb5ba7b91877
Lenny Zhou <zhoulangchun@gmail.com>
2021-04-18 16:19:35 -0400

    Implementing rockstar calculation

--- prt-backend/app.js
+++ prt-backend/app.js
@@ -6,18 +6,18 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-// const MongoClient = require('mongodb').MongoClient;
+const scheduler = require("node-schedule");
 
 const recogs = require('./mongoCalls/recognitions.js');
 const recogPeople = require('./mongoCalls/recogPeople.js');
 const user = require('./mongoCalls/user.js');
 const Employee = require('./models/employee.model.js');
+const Company = require("./models/company.model.js");
 
 const app = express();
 const { response } = require('express');
 const databaseURI = config.DATABASE_URI;
 const testFilesystemURI = config.TEST_FILESYSTEM_URI;
-console.log(testFilesystemURI);
 const sessionLength = config.SESSION_LENGTH;
 
 
@@ -130,7 +130,30 @@ app.post("/postRec", (req, res) => {
 app.use('/notifications', require('./routes/notifications'));
 app.use('/core-values', require('./routes/core-values'));
 app.use('/rockstars', require('./routes/rockstars.js'));
-app.use('/values', require('./routes/coreValues'))
+app.use('/values', require('./routes/coreValues'));
+
+const testRule = new scheduler.RecurrenceRule();
+testRule.second = [new scheduler.Range(0, 59)];
+// // const testJobInterval = "/1 * * * * *";
+// const testJob = scheduler.scheduleJob(testRule, testJobFunction);
+
+// function testJobFunction(){
+//   console.log("Tick");
+// }
+
+const rockstarRule = new scheduler.RecurrenceRule();
+rockstarRule.second = 0;
+rockstarRule.minute = 0;
+rockstarRule.hour = 0;
+rockstarRule.date = 1;
+rockstarRule.month = [new scheduler.Range(0, 11)];
+
+async function calculateRockstars(){
+  const companies = await Company.find({});
+  // console.log(companies);
+}
+
+const calculateRockstarsJob = scheduler.scheduleJob(testRule, calculateRockstars);
 
 // The call to app.listen(PORT, ...) is in server.js
 module.exports = app
